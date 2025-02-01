490a8dee7813adc1e9dec98467a071be61691b98
dleveque <d.leveque@acheterduneuf.com>
2025-02-01 15:44:17 +0100

    test api processjson and list items

--- tests/App/Controller/ApiFoodControllerTest.php
+++ tests/App/Controller/ApiFoodControllerTest.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Controller;
+namespace App\Tests\App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiFoodControllerTest extends WebTestCase
 {
@@ -18,5 +19,59 @@ class ApiFoodControllerTest extends WebTestCase
         $responseData = json_decode($client->getResponse()->getContent(), true);
         $this->assertArrayHasKey('success', $responseData);
         $this->assertTrue($responseData['success']);
+    }
+
+    public function testGetItemsWithValidType()
+    {
+
+        $client = static::createClient();
+
+
+        $client->request('GET', '/api/list?type=fruit');
+        $response = $client->getResponse();
+
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+
+
+        $this->assertJson($response->getContent());
+
+
+        $data = json_decode($response->getContent(), true);
+        $this->assertNotEmpty($data);
+    }
+
+    public function testGetItemsWithInvalidType()
+    {
+        $client = static::createClient();
+
+
+        $client->request('GET', '/api/list?type=invalid');
+        $response = $client->getResponse();
+
+
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+
+
+        $data = json_decode($response->getContent(), true);
+        $this->assertFalse($data['success']);
+        $this->assertEquals("Invalid Type of item", $data['message']);
+    }
+
+    public function testGetItemsWithoutType()
+    {
+        $client = static::createClient();
+
+
+        $client->request('GET', '/api/list');
+        $response = $client->getResponse();
+
+
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+
+
+        $data = json_decode($response->getContent(), true);
+        $this->assertFalse($data['success']);
+        $this->assertStringContainsString('Invalid Type of item', $data['message']);
     }
 }
