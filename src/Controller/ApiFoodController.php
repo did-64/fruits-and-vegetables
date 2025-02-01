96ed8a86f54c527d597e82e855c98c73769fec82
dleveque <d.leveque@acheterduneuf.com>
2025-02-01 15:45:32 +0100

    add route list items

--- src/Controller/ApiFoodController.php
+++ src/Controller/ApiFoodController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Collection\FruitCollection;
+use App\Collection\VegetableCollection;
 use App\Service\JsonFoodStorageService;
 use App\Service\StorageService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiFoodController extends AbstractController
 {
 
     public function __construct(
         private JsonFoodStorageService $foodStorage,
+        private FruitCollection $fruitCollection,
+        private VegetableCollection $vegetableCollection
     ) {}
 
     #[Route('/api/process-json', name: 'api_process_json', methods: ['GET'])]
@@ -27,13 +33,39 @@ class ApiFoodController extends AbstractController
             $storageService->setRequest($jsonRequest);
             $storageService->submitRequest();
         }catch (\Exception $exception){
-            return $this->json([
+            return new JsonResponse([
                 'success' => false,
                 'message' => $exception->getMessage()
             ], RESPONSE::HTTP_BAD_REQUEST);
         }
-        return $this->json([
+        return new JsonResponse([
             'success' => true
         ], Response::HTTP_OK);
+    }
+
+    #[Route('/api/list', name: 'api_get_items', methods: ['GET'])]
+    public function getItems(Request $request , SerializerInterface $serializer): JsonResponse
+    {
+        try {
+            $type = $request->query->get('type');
+            $list =  match ($type) {
+                'fruit' => $this->fruitCollection->list(),
+                'vegetable' => $this->vegetableCollection->list(),
+                default => null,
+            };
+            if($list === null) {
+                throw new \InvalidArgumentException("Invalid Type of item");
+            }
+            $jsonlist = $serializer->serialize($list, 'json');
+
+        }catch (\Exception $exception){
+            return new JsonResponse([
+                'success' => false,
+                'message' => $exception->getMessage()
+            ], RESPONSE::HTTP_BAD_REQUEST);
+        }
+        return new JsonResponse(
+            $jsonlist
+        , Response::HTTP_OK, [],true);
     }
 }
